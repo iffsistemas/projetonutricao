c0d3d7c307f7d17dad16bd9e32c8a3beda962cf0
Willian <Willian@DESKTOP-AJQUGMJ>
2018-05-07 23:30:29 -0300

    form taco

--- src/modelo/MotivoAtendimento.java
+++ src/modelo/MotivoAtendimento.java
@@ -1,9 +1,33 @@
 package modelo;
 
 public enum MotivoAtendimento {
-	EMAGRECIMENTO("Emagrecimento"),
-	REDALIMENTAR("Reeducação Alimentar"),
-	CONTROLCOLESTEROL("Controlar o colesterol");
+	EMAGRECIMENTO("Emagrecimento"),    
+	MANTERPESO("Manter o peso"),
+	REEDUCACAOALIMENTAR("Reeducação Alimentar"),
+	CONTROLARHIPERTENSAO("Controlar a hipertensão"),  
+	CONTROLARCOLESTEROL("Controlar o colesterol"),
+	CONTROLARGLICEMIA("Controlar a glicemia/glicose sanguínea"),
+	GANHARPESO("Ganhar peso"),  
+	EXERCICIOFISICO("Nutrição adequada ao exercício físico"),
+	CONTROLEINTESTINO("Controle do Intestino preso"),
+	CONTROLESINDROME("Controle da Síndrome do Intestino Irritável"), 
+	CONTROLETRANSTORNOS("Controle de outros transtornos do intestino"),
+	CONTROLEGASTRITE("Controle da Gastrite/Úlcera"),  
+	CONTROLEACIDOURICO("Controle do ácido úrico"), 
+	PREVENCAOCALCULO("Prevenção e/ou controle do cálculo renal"), 
+	CONTROLEREFLUXO("Controle do Refluxo gastro-esofágico"),  
+	CONTROLETPM("Controle da TPM"),
+	CONTROLEENXAQUECA("Controle da Enxaqueca"),  
+	ACOMPANHAMENTOGESTAÇÃO("Acompanhamento da Gestação"),
+	PREVENÇÃOANEMIA("Prevenção e/ou recuperação da anemia"),  
+	CONTROLEOSTEOPOROSE("Controle da Osteoporose"),
+	CIRURGIASONCOLOGICAS("Pré ou pós cirurgias oncológicas"), 
+	NUTRICAOOESTETICA("Nutrição estética – controle da celulite, acne, flacidez"),
+	INTOLERÂNCIALACTOSE("Intolerância à lactose"),  
+	DOENÇACELIACA("Alimentação para doença celíaca"),
+	NOREXIA("Anorexia/bulimia nervosa"),  
+	HIPOTIREOIDISMO("Hipotireoidismo"),
+	DOENCASCARDIOVASCULARES("Prevenção ou Controle de Doenças cardiovasculares");
 
     
 	public String descricao;
