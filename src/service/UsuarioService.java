74855469c37e9a488cd4aa9d63571717e7d4319d
Concrelagos <Concrelagos@192.168.0.105>
2018-08-06 16:53:26 -0300

    tela de login e classes usuarios

---
+++ src/service/UsuarioService.java
@@ -0,0 +1,15 @@
+package service;
+
+import javax.ejb.Stateless;
+
+import modelo.Usuario;
+
+@Stateless
+public class UsuarioService extends GenericService<Usuario> {
+
+	public UsuarioService(){
+		super(Usuario.class);
+	}
+	
+	
+}
